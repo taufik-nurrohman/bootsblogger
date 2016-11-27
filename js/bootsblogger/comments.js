c859573de96cd7e89dd680fb7ab0e2585cf26e7d
Igoy Nawamreh <igoynawamreh@gmail.com>
2016-11-27 10:47:27 +0700

    Memberikan penilaian 'false' JIKA elemen komentar TIDAK ADA.

--- js/bootsblogger/comments.js
+++ js/bootsblogger/comments.js
@@ -7,37 +7,39 @@
     //
     // Original by Taufik Nurrohman <https://plus.google.com/108949996304093815163/about>
 
-    function commentRepText(id) {
-      var a = document.getElementById(id)
-      if (!a) return
-      var b = a.innerHTML
-      var c = '//cdn.rawgit.com/tovic/dte-project/0ef9de902cf286a77272f9b3c6f8168c17ab61c6/emoticon/'
-      // Link
-      b = b.replace(/\[url=(.*?)\](.*?)\[\/url\]/ig, '<a href="$1">$2</a>')
-      // Images
-      b = b.replace(/\[img\](.*?)\[\/img\]/ig, '<img class="img-fluid" src="$1" alt="Image">')
-      // YouTube video
-      b = b.replace(/\[youtube\]https?:\/\/www\.youtube\.com\/embed\/(.*?)\[\/youtube\]/ig, '<div class="embed-responsive embed-responsive-16by9" style="margin-top: 0; margin-bottom: 0;"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/$1"><\/iframe><\/div>')
-      b = b.replace(/\[youtube\](https?:\/\/youtu\.be\/|https?:\/\/www\.youtube\.com\/watch\?v\=)(.*?)\[\/youtube\]/ig, '<div class="embed-responsive embed-responsive-16by9" style="margin-top: 0; margin-bottom: 0;"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/$2"><\/iframe><\/div>')
-      // Code & text block
-      b = b.replace(/\[code\](.*?)\[\/code\]/ig, '<code>$1<\/code>')
-      b = b.replace(/\[pre\](.*?)\[\/pre\]/ig, '<pre style="margin-top: 0; margin-bottom: 0;"><code>$1<\/code><\/pre>')
-      b = b.replace(/\[blockquote\](.*?)\[\/blockquote\]/ig, '<blockquote class="blockquote" style="margin-top: 0; margin-bottom: 0;">$1<\/blockquote>')
-      // Lists
-      b = b.replace(/\[ul\](<br>)/ig, '<ul style="margin-top: 0; margin-bottom: 0;">')
-      b = b.replace(/\[ol\](<br>)/ig, '<ol style="margin-top: 0; margin-bottom: 0;">')
-      b = b.replace(/\[ul\]/ig, '<ul style="margin-top: 0; margin-bottom: 0;">')
-      b = b.replace(/\[ol\]/ig, '<ol style="margin-top: 0; margin-bottom: 0;">')
-      b = b.replace(/\[\/ul\]/ig, '<\/ul>')
-      b = b.replace(/\[\/ol\]/ig, '<\/ol>')
-      b = b.replace(/\[li\](.*?)\[\/li\]/ig, '<li>$1<\/li>')
-      b = b.replace(/<\/li\>(<br>)/ig, '<\/li>')
-      // Finishing YouTube and Reduce filesize from images that uploaded by Blogger
-      b = b.replace(/&feature=[\w-]*/ig, '')
-      b = b.replace(/\/s(640|1600)/g, '/s400')
-      a.innerHTML = b;
+    if (document.getElementById('comments') !== null) {
+      function commentRepText(id) {
+        var a = document.getElementById(id)
+        if (!a) return
+        var b = a.innerHTML
+        var c = '//cdn.rawgit.com/tovic/dte-project/0ef9de902cf286a77272f9b3c6f8168c17ab61c6/emoticon/'
+        // Link
+        b = b.replace(/\[url=(.*?)\](.*?)\[\/url\]/ig, '<a href="$1">$2</a>')
+        // Images
+        b = b.replace(/\[img\](.*?)\[\/img\]/ig, '<img class="img-fluid" src="$1" alt="Image">')
+        // YouTube video
+        b = b.replace(/\[youtube\]https?:\/\/www\.youtube\.com\/embed\/(.*?)\[\/youtube\]/ig, '<div class="embed-responsive embed-responsive-16by9" style="margin-top: 0; margin-bottom: 0;"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/$1"><\/iframe><\/div>')
+        b = b.replace(/\[youtube\](https?:\/\/youtu\.be\/|https?:\/\/www\.youtube\.com\/watch\?v\=)(.*?)\[\/youtube\]/ig, '<div class="embed-responsive embed-responsive-16by9" style="margin-top: 0; margin-bottom: 0;"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/$2"><\/iframe><\/div>')
+        // Code & text block
+        b = b.replace(/\[code\](.*?)\[\/code\]/ig, '<code>$1<\/code>')
+        b = b.replace(/\[pre\](.*?)\[\/pre\]/ig, '<pre style="margin-top: 0; margin-bottom: 0;"><code>$1<\/code><\/pre>')
+        b = b.replace(/\[blockquote\](.*?)\[\/blockquote\]/ig, '<blockquote class="blockquote" style="margin-top: 0; margin-bottom: 0;">$1<\/blockquote>')
+        // Lists
+        b = b.replace(/\[ul\](<br>)/ig, '<ul style="margin-top: 0; margin-bottom: 0;">')
+        b = b.replace(/\[ol\](<br>)/ig, '<ol style="margin-top: 0; margin-bottom: 0;">')
+        b = b.replace(/\[ul\]/ig, '<ul style="margin-top: 0; margin-bottom: 0;">')
+        b = b.replace(/\[ol\]/ig, '<ol style="margin-top: 0; margin-bottom: 0;">')
+        b = b.replace(/\[\/ul\]/ig, '<\/ul>')
+        b = b.replace(/\[\/ol\]/ig, '<\/ol>')
+        b = b.replace(/\[li\](.*?)\[\/li\]/ig, '<li>$1<\/li>')
+        b = b.replace(/<\/li\>(<br>)/ig, '<\/li>')
+        // Finishing YouTube and Reduce filesize from images that uploaded by Blogger
+        b = b.replace(/&feature=[\w-]*/ig, '')
+        b = b.replace(/\/s(640|1600)/g, '/s400')
+        a.innerHTML = b;
+      }
+      commentRepText('comments')
     }
-    commentRepText('comments')
 
 
     // HTML converter
@@ -117,18 +119,20 @@
     // Avatar
 
     // Resize
-    var commentResizeAvatar = document.getElementById('comments')
-    var commentAvatar = commentResizeAvatar.getElementsByTagName('span')
-    if (commentAvatar) {
-      for (var i = 0; i < commentAvatar.length; i++) {
-        var s = commentAvatar.item(i).innerHTML
-        if (s.indexOf('style="') != -1) {
-          s = s.replace(/src="https:\/\/img1.blogblog.com\/img\/blank.gif"/, '')
-          s = s.replace(/src="http:\/\/img1.blogblog.com\/img\/blank.gif"/, '')
-          s = s.replace(/display: none/i, '')
-          s = s.replace(/longdesc=/i, 'src=')
-          s = s.replace(/\/s[0-9]+\//, '/s50/')
-          commentAvatar.item(i).innerHTML = s
+    if (document.getElementById('comments') !== null) {
+      var commentResizeAvatar = document.getElementById('comments')
+      var commentAvatar = commentResizeAvatar.getElementsByTagName('span')
+      if (commentAvatar) {
+        for (var i = 0; i < commentAvatar.length; i++) {
+          var s = commentAvatar.item(i).innerHTML
+          if (s.indexOf('style="') != -1) {
+            s = s.replace(/src="https:\/\/img1.blogblog.com\/img\/blank.gif"/, '')
+            s = s.replace(/src="http:\/\/img1.blogblog.com\/img\/blank.gif"/, '')
+            s = s.replace(/display: none/i, '')
+            s = s.replace(/longdesc=/i, 'src=')
+            s = s.replace(/\/s[0-9]+\//, '/s50/')
+            commentAvatar.item(i).innerHTML = s
+          }
         }
       }
     }
@@ -145,13 +149,15 @@
 //
 // Original <http://www.dte.web.id/2013/01/membuat-fitur-komentar-berbalas.html>
 
-var commentEditorIframeOriginal = document.getElementById('comment-editor').src.split('#')
-function reply(id) {
-  var form = document.getElementById('comment-form')
-  var frame = document.getElementById('comment-editor')
-  var container = (id != 'cancel') ? document.getElementById('comment-form-container-reply-' + id) : document.getElementById('comment-form-container-original')
-  var part = commentEditorIframeOriginal
+if (document.getElementById('comments') !== null) {
+  var commentEditorIframeOriginal = document.getElementById('comment-editor').src.split('#')
+  function reply(id) {
+    var form = document.getElementById('comment-form')
+    var frame = document.getElementById('comment-editor')
+    var container = (id != 'cancel') ? document.getElementById('comment-form-container-reply-' + id) : document.getElementById('comment-form-container-original')
+    var part = commentEditorIframeOriginal
 
-  container.insertBefore(form, null)
-  frame.src = (id != 'cancel') ? part[0] + '&parentID=' + id + '#' + part[1] : part[0] + '#' + part[1]
+    container.insertBefore(form, null)
+    frame.src = (id != 'cancel') ? part[0] + '&parentID=' + id + '#' + part[1] : part[0] + '#' + part[1]
+  }
 }
